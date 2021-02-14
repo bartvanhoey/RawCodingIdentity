9e55f50428d7739a320d990ea49ce533202f864e
bartvanhoey <bartvanhoey@hotmail.com>
2021-02-14 14:56:39 +0100

    Update

--- Basics/Controllers/HomeController.cs
+++ Basics/Controllers/HomeController.cs
@@ -1,27 +1,50 @@
-﻿using System.Collections.Generic;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
+﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
 
 namespace Basics.Controllers
 {
-  public class HomeController : Controller
-  {
-
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-      return View();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+
+        public IActionResult Authenticate()
+        {
+            var grandmaClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "Bob"),
+                new Claim(ClaimTypes.Email, "bob@fmail.com"),
+                new Claim("Grandma.Says", "very nice boyd")
+            };
+
+            var governmentClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "Bob K Foo"),
+                new Claim("DrivingLicense", "A+")
+            };
+
+            var grandmaIdentity = new ClaimsIdentity(grandmaClaims, "Grandma Identity");
+            var governmentIdentity = new ClaimsIdentity(governmentClaims, "Government Identity");
+
+            var userPrincipal = new ClaimsPrincipal(new[] { grandmaIdentity, governmentIdentity });
+
+            HttpContext.SignInAsync(userPrincipal);
+
+            return RedirectToAction("Index");
+        }
+
     }
-
-    [Authorize]
-    public IActionResult Secret()
-    {
-      return View();
-    }
-
-
-  }
 }
--- Basics/Startup.cs
+++ Basics/Startup.cs
@@ -1,24 +1,24 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Basics
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication("CookieAuth")
+                .AddCookie("CookieAuth", config => {
+                    config.Cookie.Name = "Grandmas.Cookie";
+                    config.LoginPath = "/Home/Authenticate";
+
+                });
+            
+            services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -28,12 +28,15 @@ namespace Basics
 
             app.UseRouting();
 
+            // who are you?
+            app.UseAuthentication();
+            
+            //are you allowed?
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
