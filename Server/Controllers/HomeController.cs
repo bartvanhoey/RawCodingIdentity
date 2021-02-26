3f4df83abc956d51715a1f1a69eb98eebfebc5c7
bartvanhoey <bartvanhoey@hotmail.com>
2021-02-26 20:21:15 +0100

    Update

---
+++ Server/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+//using Microsoft.IdentityModel.JsonWebTokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAuthorizationService _authorizationService;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+
+        public IActionResult Authenticate()
+        {
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Sub,"some_id"),
+                new Claim("Granny", "cookie")
+            };
+
+            var secretsBytes = Encoding.UTF8.GetBytes(Constants.Secret);
+            var key = new SymmetricSecurityKey(secretsBytes);
+            var algorithm = SecurityAlgorithms.HmacSha256;
+
+            var signingCredentials = new SigningCredentials(key, algorithm);
+
+            var token = new JwtSecurityToken(
+                Constants.Issuer,
+                Constants.Audience,
+                claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: signingCredentials
+              );
+
+            var tokenJson = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return Ok(new { access_token = tokenJson });
+        }
+
+
+
+        public IActionResult Decode(string part)
+        {
+            var bytes = Convert.FromBase64String(part);
+            return Ok(Encoding.UTF8.GetString(bytes));
+        }
+    }
+}
---
+++ Server/Controllers/OAuthController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    public class OAuthController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Authorize(
+            string response_type,
+            string client_id,
+            string redirect_uri,
+            string scope,
+            string state)
+        {
+            var query = new QueryBuilder
+            {
+                { "redirectUri", redirect_uri },
+                { "state", state }
+            };
+
+            return View(model: query.ToString());
+        }
+
+        [HttpPost]
+        public IActionResult Authorize(
+            string username,
+            string redirectUri,
+            string state)
+        {
+            const string code = "BBaaaaaaa";
+
+            var query = new QueryBuilder
+            {
+                { "code", code },
+                { "state", state }
+            };
+
+            return Redirect($"{redirectUri}{query}");
+        }
+
+ 
+
+        [HttpGet]
+        public async Task<IActionResult> Token(string grant_type, string code, string redirect_uri, string client_id)
+        {
+
+            // some mechanism to validating the code
+
+
+            // CREATE ACCESSTOKEN
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Sub,"some_id"),
+                new Claim("Granny", "cookie")
+            };
+
+            var secretsBytes = Encoding.UTF8.GetBytes(Constants.Secret);
+            var key = new SymmetricSecurityKey(secretsBytes);
+            var algorithm = SecurityAlgorithms.HmacSha256;
+
+            var signingCredentials = new SigningCredentials(key, algorithm);
+
+            var token = new JwtSecurityToken(
+                Constants.Issuer,
+                Constants.Audience,
+                claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: signingCredentials
+              );
+
+            var access_token = new JwtSecurityTokenHandler().WriteToken(token);
+
+            var response = new { 
+                access_token, 
+                token_type = "Bearer",
+                raw_claim = "oauthTutorial"
+            };
+
+            var jsonResponse = JsonConvert.SerializeObject(response);
+            var bytesResponse = Encoding.UTF8.GetBytes(jsonResponse);
+            await Response.Body.WriteAsync(bytesResponse, 0, bytesResponse.Length);
+
+            return Redirect(redirect_uri);
+        }
+    }
+}
